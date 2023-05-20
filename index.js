ed0697201fbfaa4a35c55fdf013a821d574412ab
ayushijain185 <125583091+ayushijain185@users.noreply.github.com>
2023-05-21 00:24:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const fs = require("fs");
+const parser = require("body-parser");
+const path = require("path");
+const app = express();
+const port = 80;
 
+app.use('/static', express.static('static'))
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine' ,'pug')
+app.set('views',path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+    res.status(200).render('home.pug');
+})
+app.get('/about',(req,res)=>{
+    res.status(200).render('about.pug');
+})
+
+app.get('/contact',(req,res)=>{
+    res.status(200).render('contact.pug');
+})
+
+app.listen(port,()=>{
+    console.log(`the application is started on ${port}`);
+})
